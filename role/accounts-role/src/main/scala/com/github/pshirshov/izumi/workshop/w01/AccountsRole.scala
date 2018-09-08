29ff895bf8d943031c6f294325cf6a44f2145f7c
pshirshov <pshirshov@gmail.com>
2018-09-08 20:26:21 +0100

    role ids, launcher commandlines

--- role/accounts-role/src/main/scala/com/github/pshirshov/izumi/workshop/w01/AccountsRole.scala
+++ role/accounts-role/src/main/scala/com/github/pshirshov/izumi/workshop/w01/AccountsRole.scala
@@ -1,8 +1,9 @@
 package com.github.pshirshov.izumi.workshop.w01
 
-import com.github.pshirshov.izumi.distage.roles.roles.RoleService
+import com.github.pshirshov.izumi.distage.roles.roles.{RoleId, RoleService}
 import com.github.pshirshov.izumi.logstage.api.IzLogger
 
+@RoleId("accounts")
 class AccountsRole(logger: IzLogger) extends RoleService {
   override def start(): Unit = {
     logger.info("Entrypoint reached: accounts role")
--- role/users-role/src/main/scala/com/github/pshirshov/izumi/workshop/w01/UsersRole.scala
+++ role/users-role/src/main/scala/com/github/pshirshov/izumi/workshop/w01/UsersRole.scala
@@ -1,9 +1,10 @@
 package com.github.pshirshov.izumi.workshop.w01
 
-import com.github.pshirshov.izumi.distage.roles.roles.RoleService
+import com.github.pshirshov.izumi.distage.roles.roles.{RoleId, RoleService}
 import com.github.pshirshov.izumi.logstage.api.IzLogger
 
-class UsersRole(logger: IzLogger) extends RoleService{
+@RoleId("users")
+class UsersRole(logger: IzLogger) extends RoleService {
   override def start(): Unit = {
     logger.info("Entrypoint reached: users role")
   }
